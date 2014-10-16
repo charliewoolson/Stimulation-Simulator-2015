f2498ea2a95300d780e5c997ea2365c0f4816e90
Bradley Gallon <bradleym007@gmail.com>
2014-10-16 13:00:32 +0000

    Temporary shader program, with basic color scheme

--- engine/engine.js
+++ engine/engine.js
@@ -62,11 +62,14 @@ function initShaders() {
     shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
     gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
 
+    shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
+    gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
+
     shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
     shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
 }
 
-function makeBuffer(vertices){
+function makePBuffer(vertices){
 	// thisVPB = thisVertexPositionBuffer
 	var thisVPB = gl.createBuffer(); // Creates the buffer
 	gl.bindBuffer(gl.ARRAY_BUFFER, thisVPB); 
@@ -74,6 +77,15 @@ function makeBuffer(vertices){
 	thisVPB.itemSize = 3; // Temporarily 3/3 for triangles, may change for other shapes, does it change for strips?
 	thisVPB.numItems = 3;
 	return thisVPB;
+}
+
+function makeCBuffer(colors){
+    var thisVCB = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, thisVCB);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+    thisVCB.itemSize = 4;
+    thisVCB.numItems = 3;
+    return thisVCB;
 }
 
 var pMatrix = mat4.create(); // perspectiv
@@ -90,26 +102,36 @@ function drawScene(items){
 	gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix); // Was previously setUniforms() line 1
 	for(i=0;i<items.length;i++){
 		mat4.translate(items[i].mvMatrix, items[i].translation); // This is what line 111 is talking about, and could be replaced
-		gl.bindBuffer(gl.ARRAY_BUFFER,items[i].buffer); // Here it uses it
-		gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, items[i].buffer.itemSize, gl.FLOAT, false, 0, 0); // and here
+		gl.bindBuffer(gl.ARRAY_BUFFER,items[i].pBuffer); // Here it uses it
+		gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, items[i].pBuffer.itemSize, gl.FLOAT, false, 0, 0); // and here
 		gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, items[i].mvMatrix); // Was previously setUniforms() line 2
-		gl.drawArrays(gl.TRIANGLES, 0, items[i].buffer.numItems); // and here
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, items[i].cBuffer);
+        gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, items[i].cBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+        gl.drawArrays(gl.TRIANGLES, 0, items[i].pBuffer.numItems); // and here
 	}
 }
 
 function initData(data){
-	// Eventually this function could be done somewhere else, but will take the data from the map file
+	// Eventually this function could be done somewhere else, but would take the data from the map file
 	data[0] = {
 		position:[
         -1.0, -1.0,  0.0,
          0.0,  1.0,  0.0,
          1.0, -1.0,  0.0
         ],
+        colors:[
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 1.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0,
+        ],
         translation:[-1.0, 0.0, -7.0],
         mvMatrix:mat4.create(), // Creates effectively a translation buffer, to use the translation
 	};
 	// It's possible could put the translation into mvMatrix here, but for now the translation is done within drawScene();
-	data[0].buffer=makeBuffer(data[0].position); // Here data is put into the buffer along with the buffer being created, is possible since this is reoccuring, could put it into the drawScene() function
+	data[0].pBuffer=makePBuffer(data[0].position); // Here data is put into the buffer along with the buffer being created, is possible since this is reoccuring, could put it into the drawScene() function
+    data[0].cBuffer=makeCBuffer(data[0].colors);
 
 	data[1] = {
 		position:[
@@ -117,11 +139,16 @@ function initData(data){
     	 1.0,  1.0,  0.0,
     	-1.0,  1.0,  0.0,
     	],
-    	buffer:makeBuffer(this.position),
+        colors:[
+        1.0, 0.0, 0.0, 1.0,
+        0.0, 1.0, 0.0, 1.0,
+        0.0, 0.0, 1.0, 1.0,
+        ],
     	translation:[1.0,0.0,-7.0],
     	mvMatrix:mat4.create(),
 	}
-	data[1].buffer=makeBuffer(data[1].position);
+	data[1].pBuffer=makePBuffer(data[1].position);
+    data[1].cBuffer=makeCBuffer(data[1].colors);
 }
 
 var triangles = [];
